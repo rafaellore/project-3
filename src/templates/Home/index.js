3ce0c9c0ef63867d30ebeb1e06a69aad79ea5a71
rafaellore <birdneon3@gmail.com>
2022-10-08 16:16:01 -0300

    adds and configure styled-components 💅

--- src/templates/Home/index.js
+++
@@ -1,9 +0,0 @@
-function Home() {
-  return (
-    <div className="App">
-      <h1>oi</h1>
-    </div>
-  );
-}
-
-export default Home;
---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,13 @@
+import * as Styled from './styles';
+
+function Home() {
+  return (
+    <div className="App">
+      <Styled.Wrapper>
+        <h1>oi</h1>
+      </Styled.Wrapper>
+    </div>
+  );
+}
+
+export default Home;
