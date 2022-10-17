dc6a28eec2f2fdbfc2d8ed8052f2f7fad709a7ea
rafaellore <birdneon3@gmail.com>
2022-10-17 17:35:17 -0300

    adds menu link component with tests

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -8,6 +8,17 @@ export const theme = {
     white: '#FFFFFF',
   },
 
+  spacings: {
+    xsmall: '0.5rem',
+    small: '1rem',
+    medium: '1.5rem',
+    large: '2rem',
+    xlarge: '2.5rem',
+    xxlarge: '3rem',
+    huge: '3.5rem',
+    xhuge: '4rem',
+  },
+
   font: {
     sizes: {
       xsmall: '0.5rem',
